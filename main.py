c103eca72570ae215a30c8d06817d620cbc1aedd
Askar Madiyev <askar.madiyev@nu.edu.kz>
2023-02-09 19:33:50 +0600

    add websocket through base64

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from typing import List
-from fastapi import FastAPI, UploadFile, WebSocket
+from fastapi import FastAPI, UploadFile, WebSocket, Body
 from tempfile import TemporaryDirectory
 import tempfile
 import model_call
 import os
+import base64
 
 app = FastAPI()
 absolute_model_path = os.path.abspath('./first_model.h5')
@@ -27,23 +28,40 @@ async def create_upload_file(file_received: List[UploadFile]):
     return {"results": emotions}
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
+@app.websocket("/ws/{filename}")
+async def websocket_endpoint(websocket: WebSocket, filename: str):
     await websocket.accept()
     while True:
-        binary_data = await websocket.receive_bytes()
-        file = convert_binary_temporary(binary_data)
+        binary_data = await websocket.receive_text()
+        file = convert_base_temporary(binary_data)
 
         with TemporaryDirectory(prefix="static-") as tmpdir:
-            new_file_path = create_new_file_path(tmpdir, file.filename)
+            new_file_path = create_new_file_path(tmpdir, filename)
             write_file_to_directory(new_file_path, file)
-            await websocket.send_text(file.filename + " : " + recognize(new_file_path))
+            await websocket.send_text(filename + " : " + recognize(new_file_path))
+
+
+@app.post("/testws")
+async def test_websocket_endpoint(name: str, binary_data: str = Body(..., example="aGVsbG8=")):
+    file = convert_base_temporary(binary_data)
+    with TemporaryDirectory(prefix="static-") as tmpdir:
+        new_file_path = create_new_file_path(tmpdir, name)
+        write_file_to_directory(new_file_path, file)
+        return name + " : " + recognize(new_file_path)
 
 
 def convert_binary_temporary(binary_data: bytes):
     file = tempfile.NamedTemporaryFile(
         max_size=1000000, mode="wb")
     file.write(binary_data)
+    file.seek(0)
+    return file
+
+
+def convert_base_temporary(data: str):
+    decoded_data = base64.b64decode(data)
+    file = tempfile.SpooledTemporaryFile(mode="wb")
+    file.write(decoded_data)
     file.seek(0)
     return file
 
