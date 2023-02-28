b1c1979ac4c7bf395847608fd09921baefa71bb7
Askar Madiyev <askar.madiyev@nu.edu.kz>
2023-02-28 22:02:45 +0600

    first get crud

--- database_pg/crud.py
+++ database_pg/crud.py
@@ -3,5 +3,5 @@ from sqlalchemy.orm import Session
 from . import models, schemes
 
 
-def get_result(db: Session, user_id: int):
-    return db.query(models)
+def get_result(db: Session, key: int):
+    return db.query(models.Result).filter(models.Result.key == key).all()
