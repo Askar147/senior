0f8679b39cebdfbb2e064b8dc2f99b45d68cee28
Askar Madiyev <askar.madiyev@nu.edu.kz>
2023-02-28 22:23:08 +0600

    upgrade model and scheme

--- database_pg/models.py
+++ database_pg/models.py
@@ -6,7 +6,7 @@ from .database import Base
 
 class Result(Base):
     __tablename__ = "results"
-
-    key = Column(String, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
+    key = Column(String, unique=False, index=True)
     order_number = Column(Integer, index=True)
     result = Column(String, index=True)
--- database_pg/schemes.py
+++ database_pg/schemes.py
@@ -2,12 +2,13 @@ from pydantic import BaseModel
 
 
 class ResultBase(BaseModel):
+    key: str
     order_number: int
     result: str
 
 
 class Result(ResultBase):
-    key: str
+    id: int
 
     class Config:
         orm_mode = True
