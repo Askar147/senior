4b3e0c3c4338d77eff029d8ca8abbcd00a14f3c9
Askar Madiyev <askar.madiyev@nu.edu.kz>
2023-03-20 01:28:53 +0600

    delete temporary endpoints

--- main.py
+++ main.py
@@ -66,35 +66,6 @@ async def create_upload_single_file(file_received: UploadFile = File(description
         return JSONResponse(emotions)
 
 
-@app.post("/api/uploadfiles/")
-async def create_upload_files(file_received: List[UploadFile] = File(description="Multiple files as UploadFile")):
-    emotions = dict()
-
-    with TemporaryDirectory(prefix="static-") as tmpdir:
-        for file in file_received:
-            mp3_audio = AudioSegment.from_file(io.BytesIO(await file.read()), format="mp3")
-            wav_audio = mp3_audio.export(format="wav")
-
-            new_file_path = create_new_file_path(tmpdir, "converted_audio.wav")
-            write_file_to_directory(new_file_path, wav_audio)
-            emotions.update({file.filename: recognize(new_file_path)})
-
-    return {"results": emotions}
-
-
-@app.post("/api/uploadfile/")
-async def create_upload_file(file_received: UploadFile = File(description="Single files as UploadFile")):
-
-    with TemporaryDirectory(prefix="static-") as tmpdir:
-        mp3_audio = AudioSegment.from_file(io.BytesIO(await file_received.read()), format="mp3")
-        wav_audio = mp3_audio.export(format="wav")
-
-        new_file_path = create_new_file_path(tmpdir, "converted_audio.wav")
-        write_file_to_directory(new_file_path, wav_audio)
-
-        return {file_received.filename: recognize(new_file_path)}
-
-
 @app.websocket("/ws/{key}")
 async def websocket_endpoint(websocket: WebSocket, key: str, db: Session = Depends(get_db)):
     await manager.connect(websocket)
@@ -119,17 +90,6 @@ async def websocket_endpoint(websocket: WebSocket, key: str, db: Session = Depen
         await manager.broadcast(f"Client disconnected")
 
 
-@app.post("/api/testws")
-async def test_websocket_endpoint(name: str, binary_data: str = Body(..., example="aGVsbG8=")):
-    file = convert_base_temporary(binary_data)
-    emotions = dict()
-    with TemporaryDirectory(prefix="static-") as tmpdir:
-        new_file_path = create_new_file_path(tmpdir, name)
-        write_file_to_directory(new_file_path, file)
-        emotions.update({name: recognize(new_file_path)})
-        return JSONResponse(emotions)
-
-
 @app.get("/api/wsresults/{key}", response_model=list[schemes.Result])
 def test_db_results(key: str, db: Session = Depends(get_db)):
     db_result = crud.get_result(db, key)
@@ -144,8 +104,8 @@ async def synchronous_token():
 
 
 @app.post("/api/wsposttest/{key}/{order}")
-def test_db_results_post(key: str, order: int, db: Session = Depends(get_db)):
-    return crud.create_result(db, key, order, "neutralize")
+def test_db_results_post(key: str, order: int, result,  db: Session = Depends(get_db)):
+    return crud.create_result(db, key, order, result)
 
 
 def generate_random_string(length):
